69fd45aa7bd54fe053c5ec99c18d1df8e07aa433
JaZahn <jcleveng@fas.harvard.edu>
2015-05-21 10:41:08 -0400

    adding a no exercises and no collections message via ng-if

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,154 +8,151 @@ angular.module('soliloquy')
     $scope.courses = [
       {
         id: null,
-        title: "All",
+        title: 'All',
         collections: []
       },
       {
         id: 1,
-        title: "Russian 1A",
+        title: 'Russian 1A',
         collections: [
           {
             id: 1,
-            title: "Week 1"
+            title: 'Week 1',
+            exercises: [
+              {
+                id: 1,
+                title: 'exercise 1'
+              },
+              {
+                id: 2,
+                title: 'exercise 2'
+              }
+            ]
           },
           {
             id: 2,
-            title: "Week 2"
+            title: 'Week 2',
+            exercises: [
+              {
+                id: 3,
+                title: 'exercise 3'
+              },
+              {
+                id: 4,
+                title: 'exercise 4'
+              }
+            ]
           },
           {
             id: 3,
-            title: "Week 3"
+            title: 'Week 3',
+            exercises: [
+              {
+                id: 5,
+                title: 'exercise 5'
+              },
+              {
+                id: 6,
+                title: 'exercise 6'
+              }
+            ]
           },
           {
             id: 4,
-            title: "Week 4"
+            title: 'Week 4',
+            exercises: [
+              {
+                id: 7,
+                title: 'exercise 7'
+              },
+              {
+                id: 8,
+                title: 'exercise 8'
+              }
+            ]
           }
         ]
       },
       {
         id: 2,
-        title: "Chinese 1B",
+        title: 'Chinese 1B',
         collections: [
           {
             id: 5,
-            title: "星期 1"
+            title: '星期 1',
+            exercises: [
+              {
+                id: 9,
+                title: '练习 1'
+              },
+              {
+                id: 10,
+                title: '练习 2'
+              }
+            ]
           },
           {
             id: 6,
-            title: "星期 2"
+            title: '星期 2',
+            exercises: [
+              {
+                id: 11,
+                title: '练习 3'
+              },
+              {
+                id: 12,
+                title: '练习 4'
+              }
+            ]
           },
           {
             id: 7,
-            title: "星期 3"
+            title: '星期 3'
           },
           {
             id: 8,
-            title: "星期 4"
+            title: '星期 4'
           }
         ]
       },
       {
         id: 3,
-        title: "Itallian 22",
+        title: 'Itallian 22',
         collections: [
           {
             id: 9,
-            title: "Nouns"
+            title: 'Nouns'
           },
           {
             id: 10,
-            title: "Verbs"
+            title: 'Verbs'
           }
         ]
       },
       {
         id: 4,
-        title: "French 33",
+        title: 'French 33',
         collections: [
           {
             id: 11,
-            title: "Week 1"
+            title: 'Week 1'
           },
           {
             id: 12,
-            title: "Week 2"
+            title: 'Week 2'
           }
         ]
       },
       {
         id: 5,
-        title: "German 44"
+        title: 'German 44'
       }
-    ];
-
-    $scope.collections = [
-      {
-        id: null,
-        title: "All"
-      },
-      {
-        id: 1,
-        title: "Week 1"
-      },
-      {
-        id: 2,
-        title: "Week 2"
-      },
-      {
-        id: 3,
-        title: "Week 3"
-      },
-      {
-        id: 4,
-        title: "Week 4"
-      },
-      {
-        id: 5,
-        title: "Nouns"
-      },
-      {
-        id: 6,
-        title: "Verbs"
-      },
-      {
-        id: 7,
-        title: "Adjectives"
-      }
-    ];
-
-    $scope.exercises = [
-      {
-        id: 1,
-        title: "exercise 1"
-      },
-      {
-        id: 2,
-        title: "exercise 2"
-      },
-      {
-        id: 3,
-        title: "exercise 3"
-      },
-      {
-        id: 4,
-        title: "exercise 4"
-      },
-      {
-        id: 5,
-        title: "exercise 5"
-      },
-      {
-        id: 6,
-        title: "exercise 6"
-      },
-
     ];
 
     $scope.courseClass = function(course){
       if($scope.current_course_id == course.id){
         if(course.id == null){
-          // build "all" collections
+          // build 'all' collections
           if(course.collections.length == 0) {
             $scope.courses.forEach(function (crs) {
               if (crs.collections) {
@@ -165,18 +162,34 @@ angular.module('soliloquy')
           }
         }
         $scope.collections = course.collections;
-        return "btn-primary";
+        return 'btn-primary';
       } else {
-        return "";
+        return '';
       }
     };
-    $scope.collectionClass = function(collection_id){
-      if($scope.current_collection_id == collection_id){
-        return "btn-primary";
+    $scope.collectionClass = function(collection){
+      if($scope.current_collection_id == collection.id){
+        if(collection.id == null){
+          console.log(collection);
+/*
+          if(collection.exercises.length == 0){
+            $scope.courses.forEach(function (crs) {
+              if (crs.collections) {
+                crs.collections.forEach(function(col){
+                  collection.exercises = collection.exercises.concat(col.exercises);
+                });
+              }
+            });
+          }
+*/
+        }
+        $scope.exercises = collection.exercises;
+        return 'btn-primary';
       } else {
-        return "";
+        return '';
       }
     };
+
 
 
 
